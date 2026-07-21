a068ea4b6e8e6c85062411305d344ea9c309fbb0
Omveer Singh <omveeralwaralwar@gmail.com>
2026-07-21 07:55:39 +0530

    Refactor code structure for improved readability and maintainability

--- obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237dcf7d826598677c9f3ffdccc17996e329810e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1405a601007f2a4a7fc5adcc69fbf522031114")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
