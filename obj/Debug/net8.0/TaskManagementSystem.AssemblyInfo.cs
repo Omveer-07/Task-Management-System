32bda700a4018b6e2b5ce7cf37505bb04e391cea
Omveer Singh <omveeralwaralwar@gmail.com>
2026-08-19 15:59:08 +0530

    hh

--- obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fd580720a17cba7620d7acbef91edf10bc709b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1b027284fa8cea7ed5dc61a91522eedcf59959")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
